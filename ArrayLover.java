cec1d1cfce4fd45b3af4486305c6af8bc8466824
owermen <omernamli91@gmail.com>
2026-01-29 09:51:47 +0300

    Menu first draft

---
+++ ArrayLover.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class ArrayLover {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("3) Find the average");
+
+        int range = in.nextInt();
+        in.nextLine();
+        int[] array = new int[range];
+
+        boolean exit = false;
+
+        while(!exit) {
+            System.out.println("-- Menu Options --");
+            System.out.println("1) Find the minimum");
+            System.out.println("2) Find the maximum");
+            System.out.println("3) Find the average");
+            System.out.println("4) Find the summ of odds and evens");
+            System.out.println("5) Exit");
+            int answer = in.nextInt();
+            in.nextLine();
+
+            if(answer == 1){
+
+                
+                //MINIMUM
+            }
+
+            if(answer == 2){
+
+                //Maximum
+            }
+
+            if(answer == 3){
+
+                //Average
+            }
+
+            if(answer == 4){
+
+                //sum of odd and even
+            }
+
+            if(answer == 5){
+                exit = true;
+                 System.out.println("Goodbye!");
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+        
+        
+
+
+
+    }
+    
+}
